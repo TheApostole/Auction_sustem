b8ecb0881dfa8f80d415ce5d8c6c2ac3770c0b54
TheApostole <yaloza99@mail.ru>
2023-07-04 05:12:21 +0300

    Курсовая работа: "Веб-разработка"

---
+++ src/main/java/coursework/webdevelopment/auction_system_cw_3/AuctionSystemCw3Application.java
@@ -0,0 +1,13 @@
+package coursework.webdevelopment.auction_system_cw_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionSystemCw3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionSystemCw3Application.class, args);
+	}
+
+}
